bee7f7f8abd031690281ba3b683cf5c9ea9533d6
vortex343 <elias.raunig@proton.me>
2025-07-07 02:02:16 +0200

    started work on ControllerListener.kt

---
+++ src/main/kotlin/ControllerListener.kt
@@ -0,0 +1,5 @@
+import kotlinx.coroutines.delay
+
+suspend fun controllerListener() {
+    print("Listener Started")
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,20 @@
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 val MACADRESS = ""
 val HTTPADRESS = "http://localhost:5001/trigger"
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "Raspi Stream Deck") {
+        val scope = rememberCoroutineScope()
+        scope.launch {
+            controllerListener()
+        }
+
         App()
     }
 }
